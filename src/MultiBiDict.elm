47f4c7bfd170928f8638b6c7e5abb1ff4eb3c442
Martin Janiczek <martin@janiczek.cz>
2019-11-05 01:01:17 +0100

    Fix comments

--- src/MultiBiDict.elm
+++ src/MultiBiDict.elm
@@ -8,8 +8,8 @@ module MultiBiDict exposing
     , union, intersect, diff, merge
     )
 
-{-| comparable1 bidirectional dictionary mapping unique keys to **multiple** values, which
-**maintains comparable1 mapping from the values back to keys.**
+{-| A bidirectional dictionary mapping unique keys to **multiple** values, which
+**maintains a mapping from the values back to keys.**
 
 Example usage:
 
@@ -69,13 +69,13 @@ import Dict.Extra
 import Set exposing (Set)
 
 
-{-| comparable1 dictionary that holds multiple values per key and allows asking for the
+{-| A dictionary that holds multiple values per key and allows asking for the
 mappings in reverse direction.
 
 Think about it as
 
     type alias BiDict comparable1 comparable2 =
-        { forward : Dict comparable1 comparable2 -- just comparable1 normal Dict!
+        { forward : Dict comparable1 comparable2 -- just a normal Dict!
         , reverse : Dict comparable2 (Set comparable1) -- the reverse mappings!
         }
 
@@ -97,7 +97,7 @@ empty =
         }
 
 
-{-| Create comparable1 dictionary with one key-value pair.
+{-| Create a dictionary with one key-value pair.
 -}
 singleton : comparable1 -> comparable2 -> MultiBiDict comparable1 comparable2
 singleton from to =
@@ -107,7 +107,7 @@ singleton from to =
         }
 
 
-{-| Insert comparable1 key-value pair into comparable1 dictionary. Replaces value when there is
+{-| Insert a key-value pair into a dictionary. Replaces value when there is
 a collision.
 -}
 insert : comparable1 -> comparable2 -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
@@ -126,7 +126,7 @@ insert from to (MultiBiDict d) =
         |> fromDict
 
 
-{-| Update the value of comparable1 dictionary for comparable1 specific key with comparable1 given function.
+{-| Update the value of a dictionary for a specific key with a given function.
 -}
 update : comparable1 -> (Set comparable2 -> Set comparable2) -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
 update from fn (MultiBiDict d) =
@@ -146,7 +146,7 @@ normalizeSet set =
         Just set
 
 
-{-| Remove comparable1 key-value pair from comparable1 dictionary. If the key is not found,
+{-| Remove a key-value pair from a dictionary. If the key is not found,
 no changes are made.
 -}
 remove : comparable1 -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
@@ -158,7 +158,7 @@ remove from (MultiBiDict d) =
         }
 
 
-{-| Determine if comparable1 dictionary is empty.
+{-| Determine if a dictionary is empty.
 
     isEmpty empty == True
 
@@ -168,15 +168,15 @@ isEmpty (MultiBiDict d) =
     Dict.isEmpty d.forward
 
 
-{-| Determine if comparable1 key is in comparable1 dictionary.
+{-| Determine if a key is in a dictionary.
 -}
 member : comparable1 -> MultiBiDict comparable1 comparable2 -> Bool
 member from (MultiBiDict d) =
     Dict.member from d.forward
 
 
-{-| Get the value associated with comparable1 key. If the key is not found, return
-`Nothing`. This is useful when you are not sure if comparable1 key will be in the
+{-| Get the value associated with a key. If the key is not found, return
+`Nothing`. This is useful when you are not sure if a key will be in the
 dictionary.
 
     animals = fromList [ ("Tom", Cat), ("Jerry", Mouse) ]
@@ -192,7 +192,7 @@ get from (MultiBiDict d) =
         |> Maybe.withDefault Set.empty
 
 
-{-| Get the keys associated with comparable1 value. If the value is not found,
+{-| Get the keys associated with a value. If the value is not found,
 return an empty set.
 -}
 getReverse : comparable2 -> MultiBiDict comparable1 comparable2 -> Set comparable1
@@ -208,7 +208,7 @@ size (MultiBiDict d) =
     Dict.foldl (\_ set acc -> Set.size set + acc) 0 d.forward
 
 
-{-| Get all of the keys in comparable1 dictionary, sorted from lowest to highest.
+{-| Get all of the keys in a dictionary, sorted from lowest to highest.
 
     keys (fromList [ ( 0, "Alice" ), ( 1, "Bob" ) ]) == [ 0, 1 ]
 
@@ -218,7 +218,7 @@ keys (MultiBiDict d) =
     Dict.keys d.forward
 
 
-{-| Get all of the values in comparable1 dictionary, in the order of their keys.
+{-| Get all of the values in a dictionary, in the order of their keys.
 
     values (fromList [ ( 0, "Alice" ), ( 1, "Bob" ) ]) == [ "Alice", "Bob" ]
 
@@ -229,35 +229,35 @@ values (MultiBiDict d) =
         |> List.concatMap Set.toList
 
 
-{-| Get comparable1 list of unique values in the dictionary.
+{-| Get a list of unique values in the dictionary.
 -}
 uniqueValues : MultiBiDict comparable1 comparable2 -> List comparable2
 uniqueValues (MultiBiDict d) =
     Dict.keys d.reverse
 
 
-{-| Get comparable1 count of unique values in the dictionary.
+{-| Get a count of unique values in the dictionary.
 -}
 uniqueValuesCount : MultiBiDict comparable1 comparable2 -> Int
 uniqueValuesCount (MultiBiDict d) =
     Dict.size d.reverse
 
 
-{-| Convert comparable1 dictionary into an association list of key-value pairs, sorted by keys.
+{-| Convert a dictionary into an association list of key-value pairs, sorted by keys.
 -}
 toList : MultiBiDict comparable1 comparable2 -> List ( comparable1, Set comparable2 )
 toList (MultiBiDict d) =
     Dict.toList d.forward
 
 
-{-| Convert comparable1 dictionary into comparable1 reverse association list of value-keys pairs.
+{-| Convert a dictionary into a reverse association list of value-keys pairs.
 -}
 toReverseList : MultiBiDict comparable1 comparable2 -> List ( comparable2, Set comparable1 )
 toReverseList (MultiBiDict d) =
     Dict.toList d.reverse
 
 
-{-| Convert an association list into comparable1 dictionary.
+{-| Convert an association list into a dictionary.
 -}
 fromList : List ( comparable1, Set comparable2 ) -> MultiBiDict comparable1 comparable2
 fromList list =
@@ -265,7 +265,7 @@ fromList list =
         |> fromDict
 
 
-{-| Apply comparable1 function to all values in comparable1 dictionary.
+{-| Apply a function to all values in a dictionary.
 -}
 map : (comparable1 -> comparable21 -> comparable22) -> MultiBiDict comparable1 comparable21 -> MultiBiDict comparable1 comparable22
 map fn (MultiBiDict d) =
@@ -274,14 +274,14 @@ map fn (MultiBiDict d) =
         |> fromDict
 
 
-{-| Convert MultiBiDict into comparable1 Dict. (Throw away the reverse mapping.)
+{-| Convert MultiBiDict into a Dict. (Throw away the reverse mapping.)
 -}
 toDict : MultiBiDict comparable1 comparable2 -> Dict comparable1 (Set comparable2)
 toDict (MultiBiDict d) =
     d.forward
 
 
-{-| Convert Dict into comparable1 MultiBiDict. (Compute the reverse mapping.)
+{-| Convert Dict into a MultiBiDict. (Compute the reverse mapping.)
 -}
 fromDict : Dict comparable1 (Set comparable2) -> MultiBiDict comparable1 comparable2
 fromDict forward =
@@ -312,7 +312,7 @@ fromDict forward =
         }
 
 
-{-| Fold over the key-value pairs in comparable1 dictionary from lowest key to highest key.
+{-| Fold over the key-value pairs in a dictionary from lowest key to highest key.
 
 
     getAges users =
@@ -329,7 +329,7 @@ foldl fn zero (MultiBiDict d) =
     Dict.foldl fn zero d.forward
 
 
-{-| Fold over the key-value pairs in comparable1 dictionary from highest key to lowest key.
+{-| Fold over the key-value pairs in a dictionary from highest key to lowest key.
 
 
     getAges users =
@@ -355,7 +355,7 @@ filter fn (MultiBiDict d) =
         |> fromDict
 
 
-{-| Partition comparable1 dictionary according to some test. The first dictionary
+{-| Partition a dictionary according to some test. The first dictionary
 contains all key-value pairs which passed the test, and the second contains
 the pairs that did not.
 -}
@@ -371,7 +371,7 @@ partition fn (MultiBiDict d) =
     )
 
 
-{-| Combine two dictionaries. If there is comparable1 collision, preference is given
+{-| Combine two dictionaries. If there is a collision, preference is given
 to the first dictionary.
 -}
 union : MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
@@ -381,7 +381,7 @@ union (MultiBiDict left) (MultiBiDict right) =
         |> fromDict
 
 
-{-| Keep comparable1 key-value pair when its key appears in the second dictionary.
+{-| Keep a key-value pair when its key appears in the second dictionary.
 Preference is given to values in the first dictionary.
 -}
 intersect : MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
@@ -391,7 +391,7 @@ intersect (MultiBiDict left) (MultiBiDict right) =
         |> fromDict
 
 
-{-| Keep comparable1 key-value pair when its key does not appear in the second dictionary.
+{-| Keep a key-value pair when its key does not appear in the second dictionary.
 -}
 diff : MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2 -> MultiBiDict comparable1 comparable2
 diff (MultiBiDict left) (MultiBiDict right) =
@@ -401,7 +401,7 @@ diff (MultiBiDict left) (MultiBiDict right) =
 
 
 {-| The most general way of combining two dictionaries. You provide three
-accumulators for when comparable1 given key appears:
+accumulators for when a given key appears:
 
 1.  Only in the left dictionary.
 2.  In both dictionaries.
